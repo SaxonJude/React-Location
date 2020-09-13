8aefe9599eaedceffb85cc8dd8562ba23a6ee437
SaxonJude <67439444+SaxonJude@users.noreply.github.com>
2020-09-13 20:36:01 +0100

    update index.js

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
-  constructor(props){
-    super(props);
+
+    state = { lat: null, err: '' };
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition( 
+            position => this.setState({ lat: position.coords.latitude }), 
+            err => this.setState({ errorMessage: err.message })
+        );
+    }
+
+    componentDidUpdate() {
+        console.log('My component was just updated - It rerendered!')
+    }
     
-    this.state = { lat: null, error: '' };
-    
-    window.navigator.getlocation.getCurrentPosition(
-    position => App.setState({ lat: position.coords.latitude }),
-    err => App.setState({ error: err.message })
-    );
-  };
-  
-  render() {
-    return (
-      <div>
-        Lattitude: {this.state.lat} <br>
-        Error: {this.state.error}
-      <div>
-    ); 
-  };
-};
-  
-ReactDOM.render(<App />, document.querySelector('#root'));
+    render() {
+        if(this.state.err && !this.state.lat) {
+        return <div>Error: {this.state.err}</div>
+        } else if (!this.state.err && this.state.lat) {
+        return <SeasonDisplay lat={this.state.lat} />
+        }
+        return <Spinner />
+    };
+}  
+
+ReactDOM.render(<App />, document.querySelector('#root')); 
